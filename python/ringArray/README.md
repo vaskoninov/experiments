be5b654dfe4517c9b1a1b70ac95b40feebb20235
Vasil Ninov <vasko.ninov@gmail.com>
2024-09-23 20:26:40 +0300

    adds

--- python/ringArray/README.md
+++ python/ringArray/README.md
@@ -1,1 +1,10 @@
+# RingBuffer class in Python
+
 A simple class implementation of a Ring Buffer in Python.
+
+Methods:
+- `__len__` - returns the current number of real elements in the array
+- `append` - appends one or more elements, if maximum size of array is reached, it will start removing the oldest elements
+- `__input_order` - returns the elements in the array by their input order, it is called by `__str__`
+- `get_elements` - returns the elements in the array in their actual positions
+- `substitute_el` - provides the ability to insert an element at a certain position in case such functionality is needed. In most common use cases such as an array for keeping logs, it won't be necessary.
--- python/ringArray/classRingBuffer.py
+++ python/ringArray/classRingBuffer.py
@@ -1,32 +1,51 @@
-class RingArray:
+class RingBuffer:
 
     def __init__(self, max_size):
         self.max_size = max_size
         self.index = 0
+        self.__current_size = 0
         self.__data = [None] * self.max_size
 
-    def __len__(self):
-        return len(self.__data)
+    def __len__(self) -> int:
+        return self.__current_size
+
+    @property
+    def max_size(self):
+        return self.__max_size
+
+    @max_size.setter
+    def max_size(self, value):
+        """
+        A setter for the max_size of the array, assuring that a correct value
+
+        for the length of the array is entered.
+        """
+        if not isinstance(value, int):
+            raise ValueError("Please insert a valid number")
+        if value <= 0:
+            raise ValueError("Please insert a number bigger than 0")
+        self.__max_size = value
 
     def append(self, *els):
         """
         An append method allowing adding more than one element
+        - if more than self.max_size elements are provided, it will start appending them
+
+        from the beginning of the array removing the first added to the list
         """
 
         for el in els:
-            self.index = (
-                self.index
-                if self.max_size > len(self.__data)
-                else self.index % self.max_size
-            )
             self.__data[self.index] = el
-            self.index += 1
+            self.index = (self.index + 1) % self.max_size
+            self.__current_size = min(self.__current_size + 1, self.max_size)
 
     def __input_order(self):
         """
-        Returns the elements in the ring array by order of input
+        Returns the elements in the ring array by order of input by the __append__ method
         - from older to most recent
         """
+        if self.__current_size < self.max_size:
+            return self.__data[: self.index]
         return self.__data[self.index :] + self.__data[: self.index]
 
     def get_elements(self):
@@ -35,18 +54,29 @@ class RingArray:
         """
         return self.__data
 
+    def substitute_el(self, value, idx):
+        """
+        A method to implement insert functionality
+        - do not use it if using the class as for relying on
+        the order and time of appending elements
+        """
+        if idx in range(0, self.max_size):
+            self.__data[idx] = value
+
     def __str__(self):
         return f"{self.__input_order()}"
 
 
 if __name__ == "__main__":
-    ring = RingArray(5)
+    ring = RingBuffer(5)
     print(len(ring))
-    ring.append("hello", "welcome", "wow", "you", "www", "11111")
+    ring.append("hello")
+    print(len(ring))
+    ring.append("hello", "welcome", "wow", "you", "www")
+    print(ring.get_elements())
     print(ring)
     ring.append("start")
     print(ring)
-    # print(ring.get())
-    ring.append("check")
-    print(ring)
     print(ring.get_elements())
+    # ring.substitute_el("gru", 3)
+    # print(ring.get_elements())
