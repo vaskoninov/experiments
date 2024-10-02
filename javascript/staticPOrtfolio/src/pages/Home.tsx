99b43c02c1374299cf1ac2063269d042b3a948c2
Vasil Ninov <vasko.ninov@gmail.com>
2024-10-02 11:11:27 +0300

    home fixes

--- javascript/staticPOrtfolio/src/pages/Home.tsx
+++ javascript/staticPOrtfolio/src/pages/Home.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
     return (
         <>
-            <h1>Home</h1>
+            <div className="flex flex-row w-full h-screen">
+                <div className="w-1/2 bg-slate-400">
+                    <img src="" alt="My personal photo" />
+                </div>
+                <div className="w-1/2">
+                    Hello, My name is Vasil Ninov and I am happy to present
+                    myself and my software projects.
+                </div>
+            </div>
         </>
     );
 }
