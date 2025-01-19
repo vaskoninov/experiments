a748a16bdcb83689752ec8c761ac386a0ff1af5d
Vasil Ninov <vasko.ninov@gmail.com>
2025-01-19 18:17:26 +0200

    new app

--- python/flaskMicroBlog/app/__init__.py
+++ python/flaskMicroBlog/app/__init__.py
@@ -3,3 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 from app import routes
+
+
+# export FLASK_APP=microblog.py -> in order flask to know about the app and how to run it
+# flask run -> to start the app
+# flask run --port 5001 -> to run it on a port different from the default one 5000
