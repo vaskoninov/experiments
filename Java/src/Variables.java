99fd46c9d9e64403421e939b384e65f05ebebd29
Vasil Ninov <vasko.ninov@gmail.com>
2025-02-17 22:36:46 +0200

    new

---
+++ Java/src/Variables.java
@@ -0,0 +1,35 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age;
+        age = 10;
+        System.out.println(age);
+
+        double score;
+        score = 19.4;
+
+        System.out.println(score);
+
+        char grade;
+        grade = 'A';
+
+        System.out.println(grade);
+
+        boolean isFound;
+        isFound = false;
+
+        System.out.println(isFound);
+
+        if (!isFound) {
+            System.out.println("false");
+        }
+        else {
+            System.out.println("true");
+        }
+
+        String name;
+        name = "Pete";
+
+        System.out.println(name);
+
+    }
+}
